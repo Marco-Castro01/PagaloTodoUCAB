e2d945c31d35fadb3e4d7101a80790d28bd157b9
Marco-Castro01 <castroo8a2@gmail.com>
2023-05-21 12:28:34 -0400

    AgregarPago

--- src/pagalotodo-ucab-ms/UCABPagaloTodoMS.Application/Responses/PagoResponse.cs
+++ src/pagalotodo-ucab-ms/UCABPagaloTodoMS.Application/Responses/PagoResponse.cs
@@ -6,7 +6,10 @@ namespace UCABPagaloTodoMS.Application.Responses
     {
         public Guid Id { get; set; }
         public double? valor { get; set; }
-        public ServicioEntity servicioId { get; set; }
-        public ConsumidorEntity consumidorId { get; set; }
+        public Guid servicioId { get; set; }
+        public string? NombreServicio { get; set; }
+        public string? PrestadorServicioNombre { get; set; }
+        public Guid consumidorId { get; set; }
+        public string NombreConsumidor { get; set; }
     }
 }
