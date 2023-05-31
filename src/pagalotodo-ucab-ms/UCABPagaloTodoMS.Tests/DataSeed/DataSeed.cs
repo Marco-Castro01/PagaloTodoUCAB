e22c3272c27e967309a570679ec8c59fa8893df6
raymond311arteaga <87507496+raymond311arteaga@users.noreply.github.com>
2023-05-31 10:53:19 -0400

    Usuario controller arreglado y Prueba Data seed

--- src/pagalotodo-ucab-ms/UCABPagaloTodoMS.Tests/DataSeed/DataSeed.cs
+++ src/pagalotodo-ucab-ms/UCABPagaloTodoMS.Tests/DataSeed/DataSeed.cs
@@ -1,13 +1,130 @@
-﻿using Moq;
+﻿using Microsoft.EntityFrameworkCore;
 using UCABPagaloTodoMS.Core.Database;
+using MockQueryable.Moq;
+using Moq;
+using UCABPagaloTodoMS.Infrastructure.Database;
+using UCABPagaloTodoMS.Core.Entities;
+using UCABPagaloTodoMS.Infrastructure.Migrations;
+
 
 namespace UCABPagaloTodoMS.Tests.DataSeed
 {
     public static class DataSeed
     {
+        public static Mock<DbSet<AdminEntity>> mockSetAdminEntity = new Mock<DbSet<AdminEntity>>();
+        public static Mock<DbSet<BaseEntity>> mockSetBaseEntity = new Mock<DbSet<BaseEntity>>();
+        public static Mock<DbSet<CamposConciliacionEntity>> mockSetCamposConciliacionEntity = new Mock<DbSet<CamposConciliacionEntity>>();
+    public static Mock<DbSet<ConsumidorEntity>> mockSetConsumidorEntity = new Mock<DbSet<ConsumidorEntity>>();
+    public static Mock<DbSet<PagoEntity>> mockSetSPagoEntity = new Mock<DbSet<PagoEntity>>();
+    public static Mock<DbSet<PrestadorServicioEntity>> mockSetPrestadorServicioEntity = new Mock<DbSet<PrestadorServicioEntity>>();
+    public static Mock<DbSet<ServicioEntity>> mockSetServicioEntity = new Mock<DbSet<ServicioEntity>>();
+    public static Mock<DbSet<UsuarioEntity>> mockSetUsuarioEntity = new Mock<DbSet<UsuarioEntity>>();
+    public static Mock<DbSet<ValoresEntity>> mockSetValoresEntity = new Mock<DbSet<ValoresEntity>>();
+
+
         public static void SetupDbContextData(this Mock<IUCABPagaloTodoDbContext> mockContext)
         {
 
+            var Admin = new List<AdminEntity>
+            {
+                new AdminEntity
+                {
+                    cedula = "123456789"
+                },
+
+
+            };
+
+            var Base = new List<BaseEntity>
+            {
+                 new BaseEntity
+                {
+                   // Id = 1,
+                    CreatedBy = "Bismarck",
+                    UpdatedAt = DateTime.Now,
+                    
+                },
+
+
+
+            };
+            var CamposConciliacion = new List<CamposConciliacionEntity>
+            {
+                 new CamposConciliacionEntity
+                {
+                    Nombre = "prueba",
+                    Longitud = 2,
+                    //Servicio
+                },
+
+
+
+            };
+            var Consumidor = new List<ConsumidorEntity>
+            {
+                 new ConsumidorEntity
+                {
+                    name = "Prueba1",
+                    lastName = "Arteaga",
+                    cedula = "123456789",
+
+                },
+
+
+
+            };
+            var Pagos = new List<PagoEntity>
+            {
+                 new PagoEntity
+                {
+                    cedula = "123456789"
+                },
+
+
+
+            };
+            var Prestador = new List<PrestadorServicioEntity>
+            {
+                 new PrestadorServicioEntity
+                {
+                    cedula = "123456789"
+                },
+
+
+
+            };
+            var Servicio = new List<ServicioEntity>
+            {
+                 new ServicioEntity
+                {
+                    cedula = "123456789"
+                },
+
+
+
+            };
+            var Usuario = new List<UsuarioEntity>
+            {
+                 new UsuarioEntity
+                {
+                    cedula = "123456789"
+                },
+
+
+
+            };
+            var Valores = new List<ValoresEntity>
+            {
+                 new ValoresEntity
+                {
+                    cedula = "123456789"
+                },
+
+
+
+            };
+
+
+
         }
     }
-}
--- src/pagalotodo-ucab-ms/UCABPagaloTodoMS.Tests/UnitTestsApplication/Handlers/Commands/AgregarAdminPruebaCommandHandlerTest.cs
+++ src/pagalotodo-ucab-ms/UCABPagaloTodoMS.Tests/UnitTestsApplication/Handlers/Commands/AgregarAdminPruebaCommandHandlerTest.cs
@@ -1,12 +1,132 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Extensions.Logging;
+using Moq;
+using UCABPagaloTodoMS.Core.Database;
+using UCABPagaloTodoMS.Tests.DataSeed;
+using UCABPagaloTodoMS.Application.Commands;
+using UCABPagaloTodoMS.Application.Handlers.Commands;
+using UCABPagaloTodoMS.Application.Handlers.Queries;
+using Microsoft.EntityFrameworkCore.Storage;
+using System.Reflection.Metadata;
+using Xunit;
 
-namespace UCABPagaloTodoMS.Tests.UnitTestsApplication.Handlers.Commands
+
+namespace UCABPagaloTodoMS.Tests.UnitTestsApplication.Handlers.Commands.AdminPrueba
 {
-    public class AgregarAdminPruebaCommandHandlerTest
+    public class AgregarAdminPruebaCommandHandlerTests
     {
-    }
+
+        private readonly AgregarAdminPruebaCommandHandler _handler;
+        private readonly Mock<IUCABPagaloTodoDbContext> _contextMock;
+        private readonly Mock<ILogger<ConsultarAdminQueryHandler>> _mockLogger;
+        private readonly Mock<IDbContextTransactionProxy> _mockProxy;
+
+        public AgregarAdminPruebaCommandHandlerTests()
+        {
+            _mockLogger = new Mock<ILogger<ConsultarAdminQueryHandler>>();
+            _contextMock = new Mock<IUCABPagaloTodoDbContext>();
+            _mockProxy = new Mock<IDbContextTransactionProxy>();
+            _handler = new AgregarAdminPruebaCommandHandler(_contextMock.Object, _mockLogger.Object);
+            _contextMock.SetupDbContextData();
+        }
+
+        [Fact]
+        public async Task Handle_ReturnsExpectedGuid_WhenRequestIsValid()
+        {
+            // Arrange
+            var request = new AgregarAdminPruebaCommand { /* initialize properties */ };
+            var expectedGuid = Guid.NewGuid();
+            
+public class AgregarAdminPruebaCommandHandlerTests
+        {
+            private readonly Mock<IUCABPagaloTodoDbContext> _dbContextMock;
+            private readonly Mock<ILogger<ConsultarAdminQueryHandler>> _loggerMock;
+            private readonly AgregarAdminPruebaCommandHandler _agregarAdminPruebaCommandHandler;
+
+            public AgregarAdminPruebaCommandHandlerTests()
+            {
+                _dbContextMock = new Mock<IUCABPagaloTodoDbContext>();
+                _loggerMock = new Mock<ILogger<ConsultarAdminQueryHandler>>();
+                _agregarAdminPruebaCommandHandler = new AgregarAdminPruebaCommandHandler(_dbContextMock.Object, _loggerMock.Object);
+            }
+
+            [Fact]
+            public async Task Handle_ReturnsExpectedGuid_WhenRequestIsValid()
+            {
+                // Arrange
+                var request = new AgregarAdminPruebaCommand { /* initialize properties */ };
+                var expectedGuid = Guid.NewGuid();
+                
+public class AgregarAdminPruebaCommandHandlerTests
+            {
+                private readonly Mock<IUCABPagaloTodoDbContext> _dbContextMock;
+                private readonly Mock<ILogger<AgregarAdminPruebaCommandHandler>> _loggerMock;
+                private readonly AgregarAdminPruebaCommandHandler _agregarAdminPruebaCommandHandler;
+
+                public AgregarAdminPruebaCommandHandlerTests()
+                {
+                    _dbContextMock = new Mock<IUCABPagaloTodoDbContext>();
+                    _loggerMock = new Mock<ILogger<AgregarAdminPruebaCommandHandler>>();
+                    _agregarAdminPruebaCommandHandler = new AgregarAdminPruebaCommandHandler(_dbContextMock.Object, _loggerMock.Object);
+                }
+
+                [Fact]
+                public async Task Handle_ReturnsExpectedGuid_WhenRequestIsValid()
+                {
+                    // Arrange
+                    var request = new AgregarAdminPruebaCommand { /* initialize properties */ };
+                    var expectedGuid = Guid.NewGuid();
+                    _dbContextMock
+                        .Setup(m => m.BeginTransaction())
+                        .Returns(new Mock<IDbContextTransaction>().Object);
+                    _dbContextMock
+                        .Setup(m => m.Admin.Add(It.IsAny<Admin>()))
+                        .Callback<Admin>(e =>
+                        {
+                            e.IdSigo 
+            
+                    _dbContextMock
+                        .Setup(m => m.BeginTransaction())
+                        .Returns(new Mock<IDbContextTransaction>().Object);
+                            _dbContextMock
+                    .Setup(m => m.Admin.Add(It.IsAny<Admin>()))
+                    .Callback<Admin>(e =>
+                        {
+                            e.Id = expectedGuid;
+                        });
+                            _dbContextMock
+                    .Setup(m => m.SaveEfContextChanges("APP"))
+                    .Returns(Task.CompletedTask);
+
+                            // Act
+                            var actualGuid = await _agregarAdminPruebaCommandHandler.Handle(request, CancellationToken.None);
+
+                            // Assert
+                            Assert.Equal(expectedGuid, actualGuid);
+                        }
+
+                    [Fact]
+                public async Task Handle_ThrowsException_WhenDbContextThrowsException()
+                    {
+                        // Arrange
+                        var request = new AgregarAdminPruebaCommand { /* initialize properties */ };
+                        _dbContextMock
+                            .Setup(m => m.BeginTransaction())
+                            .Returns(new Mock<IDbContextTransaction>().Object);
+                        _dbContextMock
+                            .Setup(m => m.Admin.Add(It.IsAny<Admin>()))
+                            .ThrowsException();
+                        _dbContextMock
+                            .Setup(m => m.SaveEfContextChanges("APP"))
+                            .Returns(Task.CompletedTask);
+
+                        // Act and Assert
+                        await Assert.ThrowsAsync<Exception>(() => _agregarAdminPruebaCommandHandler.Handle(request, CancellationToken.None));
+                    }
+                }
+
+
+            }
+
+
 }
+    }
