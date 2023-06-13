bedcf156bf2f009ebeb068f5505b7cafb7895ef1
Marco-Castro01 <86668466+Marco-Castro01@users.noreply.github.com>
2023-06-12 22:02:59 -0400

    feat[DES-140] Asignacion a los servicios de los campos que estaran en el archivo de conciliacion

--- src/pagalotodo-ucab-ms/UCABPagaloTodoMS.Application/CustomExceptions/CustomException.cs
+++ src/pagalotodo-ucab-ms/UCABPagaloTodoMS.Application/CustomExceptions/CustomException.cs
@@ -1,22 +1,63 @@
-﻿namespace UCABPagaloTodoMS.Application.CustomExceptions;
+﻿using Microsoft.Azure.Amqp.Framing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using FluentValidation;
 
-public class CustomException : Exception
+namespace UCABPagaloTodoMS.Application.CustomExceptions
 {
-    public CustomException()
+    public class CustomException : Exception
     {
+        public int Codigo { get; set; }
+
+        public CustomException()
+        {
+        }
+
+        public CustomException(string message)
+            : base(message)
+        {
+        }
+        public CustomException(int codigo, string message)
+            : base(message)
+        {
+            Codigo = codigo;
+        }
+
+        public CustomException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+        
+        public CustomException(int codigo,string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+
+
+
+        public CustomException(int codigo, string mensaje, Exception innerException, List<Error> errores)
+            : base(mensaje, innerException)
+        {
+            Codigo = codigo;
+        }
+
+        public static CustomException CrearDesdeListaException(int codigo, string mess,ValidationException ex)
+        {
+            string mensaje = $"{mess}: {Environment.NewLine}";
+
+            foreach (var error in ex.Errors.OrderBy(e => e.PropertyName))
+            {
+                mensaje += $"- {error.PropertyName}: {error.ErrorMessage}{Environment.NewLine}";
+            }
+
+            return new CustomException(codigo, mensaje, ex, ex.Errors.Select(e => new Error { Campo = e.PropertyName, Mensaje = e.ErrorMessage }).ToList());
+        }
     }
 
-    public CustomException(string message)
-        : base(message)
+    public class Error
     {
-    }
-
-    public CustomException(string message, Exception inner)
-        : base(message, inner)
-    {
-    }
-    public override string ToString()
-    {
-        return $"{typeof(CustomException).FullName}: {Message}\n{StackTrace}";
+        public string Campo { get; set; }
+        public string Mensaje { get; set; }
     }
 }
